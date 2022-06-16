8a70b01ef60de3e5b4fedfcff61b1280aa8bd0c6
FaiyaazHawk <faiyaaz.haque90@gmail.com>
2022-06-16 19:59:50 -0400

    rubocop pass

--- linkedlist.rb
+++ linkedlist.rb
@@ -1,149 +1,146 @@
-class Node
-    attr_accessor :value, :nextNode
+# frozen_string_literal: true
 
-    def initialize(value = nil,nextNode = nil)
-        @value = value
-        @nextNode = nextNode
-    end
+class Node
+  attr_accessor :value, :nextNode
+
+  def initialize(value = nil, nextNode = nil)
+    @value = value
+    @nextNode = nextNode
+  end
 end
 
 class Linkedlist
-    attr_accessor :head
-    def initialize
-        @head = nil
+  attr_accessor :head
+  def initialize
+    @head = nil
+  end
+
+  def append(value)
+    if @head.nil?
+      @head = Node.new(value, nil)
+    else
+      lastNode = @head
+      lastNode = lastNode.nextNode until lastNode.nextNode.nil?
+      lastNode.nextNode = Node.new(value, nil)
+    end
+  end
+
+  def prepend(value)
+    newHead = Node.new(value)
+    newHead.nextNode = @head
+    @head = newHead
+  end
+
+  def size
+    count = 1
+    if head.nextNode.nil?
+      return count
+    else
+      currentNode = @head
+      until currentNode.nextNode.nil?
+        count += 1
+        currentNode = currentNode.nextNode
+      end
     end
 
-    def append(value)
-        if @head.nil?
-            @head = Node.new(value,nil)
-        else
-            lastNode = @head
-            until lastNode.nextNode.nil?
-                lastNode = lastNode.nextNode
-            end
-            lastNode.nextNode = Node.new(value,nil)
-        end
+    count
+  end
+
+  attr_reader :head
+
+  def tail
+    lastNode = @head
+    lastNode = lastNode.nextNode until lastNode.nextNode.nil?
+    lastNode
+  end
+
+  def at(index)
+    currentNode = @head
+    currentindex = 0
+    if index == 0
+      return @head
+    else
+      until currentindex == index
+        currentindex += 1
+        currentNode = currentNode.nextNode
+      end
     end
 
-    def prepend(value)
-        newHead = Node.new(value)
-        newHead.nextNode = @head
-        @head = newHead
-    end
+    currentNode
+  end
 
-    def size
-        count = 1
-        if head.nextNode.nil?
-            return count
-        else
-            currentNode = @head
-            until currentNode.nextNode.nil?
-                count +=1
-                currentNode = currentNode.nextNode
-            end
-        end
-        return count        
+  def pop
+    prevNode = nil
+    endNode = @head
+    until endNode.nextNode.nil?
+      prevNode = endNode
+      endNode = endNode.nextNode
     end
+    prevNode.nextNode = nil
+    endNode
+  end
 
-    def head
-        return @head
+  def contains?(value)
+    currentNode = @head
+    until currentNode.nextNode.nil?
+      if currentNode.value == value
+        return true
+      else
+        currentNode = currentNode.nextNode
+      end
     end
+    false
+  end
 
-    def tail
-        lastNode = @head
-        until lastNode.nextNode.nil?
-            lastNode = lastNode.nextNode
-        end
-        return lastNode
+  def find(value)
+    currentNode = @head
+    currentindex = 0
+    until currentNode.nextNode.nil?
+      if currentNode.value == value
+        return currentindex
+      else
+        currentNode = currentNode.nextNode
+        currentindex += 1
+      end
     end
+    nil
+  end
 
-    def at(index)
-        currentNode = @head
-        currentindex = 0
-        if index == 0
-            return @head
-        else
-            until currentindex == index
-                currentindex += 1
-                currentNode = currentNode.nextNode
-            end
-        end
-        return currentNode
+  def to_s
+    output = ''
+    currentNode = @head
+    until currentNode.nil?
+      output << "(#{currentNode.value}) ->"
+      currentNode = currentNode.nextNode
     end
+    output << 'nil'
+    output
+  end
 
-    def pop
-        prevNode = nil
-        endNode = @head
-        until endNode.nextNode.nil?
-            prevNode = endNode
-            endNode = endNode.nextNode
-        end
-        prevNode.nextNode = nil
-        return endNode
+  def insert_at(value, index)
+    if index > size
+      puts 'index greater than list size'
+    elsif index == size
+      append(value)
+    elsif index == 0
+      prepend(value)
+    else
+      newNode = Node.new(value, at(index))
+      prevNode = at(index - 1)
+      prevNode.nextNode = newNode
     end
+  end
 
-    def contains?(value)
-        currentNode = @head
-        until currentNode.nextNode.nil?
-            if currentNode.value == value
-                return true
-            else
-                currentNode = currentNode.nextNode
-            end
-        end
-        return false 
+  def remove_at(index)
+    if index >= size
+      puts 'index greater than list size'
+    elsif index == 0
+      @head = at(1)
+    else
+      preNode = at(index - 1)
+      preNode.nextNode = preNode.nextNode.nextNode
     end
-
-    def find(value)
-        currentNode = @head
-        currentindex = 0
-        until currentNode.nextNode.nil?
-            if currentNode.value == value
-                return currentindex
-            else
-                currentNode = currentNode.nextNode
-                currentindex += 1
-            end
-        end
-        return nil
-    end
-
-    def to_s
-        output = ''
-        currentNode = @head
-        until currentNode.nil?
-            output << "(#{currentNode.value}) ->"
-            currentNode = currentNode.nextNode
-        end
-        output << 'nil'
-        return output
-    end
-
-    def insert_at(value, index)
-        if index > self.size
-            return puts "index greater than list size"
-        elsif (index) == self.size
-            self.append(value)
-        elsif index == 0
-            self.prepend(value)
-        else
-            newNode = Node.new(value,self.at(index))
-            prevNode = self.at(index-1)
-            prevNode.nextNode = newNode
-        end
-    end
-
-    def remove_at(index)
-        if index >= self.size
-            return puts "index greater than list size"
-        elsif index == 0
-            @head = self.at(1)
-        else
-            preNode = self.at(index-1)
-            preNode.nextNode = preNode.nextNode.nextNode
-        end
-    end
-
+  end
 end
 
 ll = Linkedlist.new
